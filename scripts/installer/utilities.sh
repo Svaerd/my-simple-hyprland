b83812d871cd3801948d5d48401bc41c487d0664
Svaerd <danishhusam@gmail.com>
2025-08-17 09:40:57 +0700

    misc

--- scripts/installer/utilities.sh
+++ scripts/installer/utilities.sh
@@ -10,10 +10,10 @@ log_message "Installation started for utilities section"
 print_info "\nStarting utilities setup..."
 
 run_command "yay -S --noconfirm hyprpanel" "Install Hyprpanel - Status Bar" "yes"
-run_command "ln --symbolic $BASE_DIR/configs/hyprpanel /home/$SUDO_USER/.config/" "Copy Hyprpanel config" "yes" "no"
+run_command "ln --symbolic $BASE_DIR/configs/hyprpanel /home/$SUDO_USER/.config/" "Symlink Hyprpanel config" "yes" "no"
 
 run_command "yay -S --sudoloop --noconfirm rofi" "Install rofi - Application Launcher" "yes" "no"
-run_command "ln --symbolic $BASE_DIR/configs/rofi /home/$SUDO_USER/.config/" "Copy rofi config(s)" "yes" "no"
+run_command "ln --symbolic $BASE_DIR/configs/rofi /home/$SUDO_USER/.config/" "Symlink rofi config(s)" "yes" "no"
 
 run_command "pacman -S --noconfirm cliphist" "Install Cliphist - Clipboard Manager" "yes"
 
@@ -22,10 +22,10 @@ run_command "yay -S --sudoloop --noconfirm swww waypaper" "Install SWWW and Wayp
 run_command "yay -S --sudoloop --noconfirm hyprpicker" "Install Hyprpicker - Color Picker" "yes" "no"
 
 run_command "yay -S --sudoloop --noconfirm hyprlock" "Install Hyprlock - Screen Locker (Must)" "yes" "no"
-run_command "ln --symbolic $BASE_DIR/configs/hypr/hyprlock.conf /home/$SUDO_USER/.config/hypr/" "Copy Hyprlock config" "yes" "no"
+run_command "ln --symbolic $BASE_DIR/configs/hypr/hyprlock.conf /home/$SUDO_USER/.config/hypr/" "Symlink Hyprlock config" "yes" "no"
 
 run_command "yay -S --sudoloop --noconfirm wlogout" "Install Wlogout - Session Manager" "yes" "no"
-run_command "ln --symbolic $BASE_DIR/configs/wlogout /home/$SUDO_USER/.config/ && ln --symbolic $BASE_DIR/assets/wlogout /home/$SUDO_USER/.config/assets/" "Copy Wlogout config and assets" "yes" "no"
+run_command "ln --symbolic $BASE_DIR/configs/wlogout /home/$SUDO_USER/.config/ && ln --symbolic $BASE_DIR/assets/wlogout /home/$SUDO_USER/.config/assets/" "Symlink Wlogout config and assets" "yes" "no"
 
 run_command "yay -S --sudoloop --noconfirm grimblast" "Install Grimblast - Screenshot tool" "yes" "no"
 
