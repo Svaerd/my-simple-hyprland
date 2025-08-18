7a7c35a859fb6d6e76686b34ed1f82eca7a7ebbf
dann <husam.danish@proton.me>
2025-08-18 22:30:59 +0700

    revamp
    remove gnome extension, add gtk-engine-murrine, fix priv issue, change
    theme, comment out some lines

--- scripts/installer/theming.sh
+++ scripts/installer/theming.sh
@@ -12,27 +12,30 @@ print_info "\nStarting theming setup..."
 # Bibata cursor
 run_command "tar -xvf $BASE_DIR/assets/cursor/Bibata-Modern-Ice.tar.xz -C /usr/share/icons/" "Install Bibata-Modern-Ice as the cursor theme" "yes"
 
-# Gnome extension
-# Openbar, for theming
-run_command "ln --symbolic $BASE_DIR/assets/extension/openbar@neuromorph /home/$SUDO_USER/.local/share/gnome-shell/extensions"
 # SDDM astro theme
 run_command "yay -S --sudoloop --noconfirm sddm-astronaut-theme" "install SDDM Theme" "yes" "no"
 run_command "ln --symbolic $BASE_DIR/configs/sddm/sddm.conf /etc/ " "Apply SDDM Theme" "yes"
 
-# run_command "pacman -S --noconfirm nwg-look" "Install nwg-look for GTK theme management" "yes" "no"
-#
-# run_command "pacman -S --noconfirm qt5ct qt6ct kvantum" "Install Qt5, Qt6 Settings, and Kvantum theme engines" "yes" "no"
-#
-# run_command "tar -xvf $BASE_DIR/assets/themes/Catppuccin-Mocha.tar.xz -C /usr/share/themes/" "Install Catppuccin Mocha GTK theme" "yes"
-#
-# run_command "tar -xvf $BASE_DIR/assets/icons/Tela-circle-dracula.tar.xz -C /usr/share/icons/" "Install Tela Circle Dracula icon theme" "yes"
+run_command "pacman -S --noconfirm gtk-engine-murrine" "require to apply gtk2 themes" "yes"
+
+run_command "pacman -S --noconfirm nwg-look" "Install nwg-look for GTK theme management" "yes"
+
+# Kvantum
+run_command "pacman -S --noconfirm qt5ct qt6ct kvantum" "Install Qt5, Qt6 Settings, and Kvantum theme engines" "yes"
+run_command "ln --symbolic $BASE_DIR/configs/Kvantum /home/$SUDO_USER/.config/ " "Apply Kvantum Theme" "yes" "no"
+
+# GTK theme
+run_command "7z x $BASE_DIR/assets/themes/Catppuccin-Dark-Macchiato.zip -o/usr/share/themes/" "Move Catppuccin Macchiato GTK theme to themes directory" "yes"
+
+# Tela-circle-dracula
+# run_command "tar -xvf $BASE_DIR/assets/icons/Tela-circle-dracula.tar.xz -C /usr/share/icons/" "Move Tela Circle Dracula icon theme to icons directory" "yes"
 #
 # run_command "yay -S --sudoloop --noconfirm kvantum-theme-catppuccin-git" "Install Catppuccin theme for Kvantum" "yes" "no"
 
 # Add instructions to configure theming
 # print_info "\nPost-installation instructions:"
 # print_bold_blue "Set themes and icons:"
-# echo "   - Run 'nwg-look' and  set the global GTK and icon theme"
+print_bold_blue "   - Run 'nwg-look' and  set the global GTK and icon theme"
 # echo "   - Open 'kvantummanager' (run with sudo for system-wide changes) to select and apply the Catppuccin theme"
 # echo "   - Open 'qt6ct' to set the icon theme"
 
